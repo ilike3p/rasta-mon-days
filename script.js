46388020d22a67db2291675bf0b0b2d1c15c5cf4
ilike3p <39431924+ilike3p@users.noreply.github.com>
2022-12-14 21:33:53 -0800

    added timeblock section

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-//Time section for variables
-$(document).ready(function() {
-  var currentDate = moment().format("MMMM Do, YYYY");
-  var currentTime = moment().format("hh:mm:ss A");
-  var currentHour;
-  var possibleHours = {
-    before: ["12AM","01AM","02AM","03AM","04AM","05AM","06AM","07AM","08AM"],
-    business: ["09AM", "10AM", "11AM", "12PM", "01PM", "02PM", "03PM", "04PM", "05PM" ],
-    after: ["06PM", "07PM", "08PM", "09PM", "10PM", "11PM"]
 
 
-  }
-}
-function init() {
-  // Display current date
-  $("#date").text(currentDate);
-  // Display current time
-  $("#time").text(currentTime);
-  // Set colors based on current time
-  timeColor();
-  // Update current date and time every second
-  let currentTimer = setInterval(function() {
-    currentDate = moment().format("MMMM Do, YYYY");
-    $("#date").text(currentDate);
-    currentTime = moment().format("hh:mm:ss A");
-    $("#time").text(currentTime);
-    timeColor();
-  }, 1000);
-}
+// Display for today's date and time 
+const todayDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+$("#currentDay").html(todayDate);
 
-//Change color of text/box past/present/future
-function timeColor() {
-  currentHour = moment().format("hhA");
-  //If currentHours is before 9am set all hours to background color to gray
-  if (possibleHours.before.indexOf(currentHour) !==-1){
-      $(".hourNotes").css("background-color", "#d3d3d3");
-  }
-  
+// Function for click listener on saveBtn
+$(document).ready(function (){
+    $('.saveBtn').on('click', function (){
+        const text = $(this).siblings('.description').val();
+        const time = $(this).parent().attr('id');
+
+        // Save local time and text input in local storage
+        localStorage.setItem(time, text);
+    })
+
+    // Function to calculate current dayjs hour for timeblock
+    function timeTracker (){
+        const currentTime = dayjs().hour();
+
+    // Function to loop over each timeblock
+    $('.time-block').each(function(){
+        const blockTime = parseInt($(this).attr('id').split('hour')[1]);
+
+        // Calculate current time and add past class for grey timeblock colour
+        if (blockTime < currentTime){
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+            $(this).addClass('past');
+        }
+
+
